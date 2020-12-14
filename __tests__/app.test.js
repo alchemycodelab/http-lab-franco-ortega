9148a554607f73eb45406f609da34c0a442cea1f
Franco Ortega <francopdx@gmail.com>
2020-12-14 15:19:16 -0800

    add GET test for first endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,15 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+//   it('sample fake test', () => {
+//     expect(true).toEqual(true);
+//   });
+
+  it('GET hi from /', async() => {
+    const response = await request(app)
+      .get('/');
+
+    expect(response).toEqual('hi');
+  });
 
 });
